82aeca3127df563152f10ee802b18723fc382efe
pumisj <83750080+pumisj@users.noreply.github.com>
2022-06-19 14:04:52 +0900

    Add files via upload

--- fadein.js
+++ fadein.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $(window).scroll( function(){
-        $('.des1').each( function(i){
+$(document).ready(function() { //document라는 객체의 준비가 끝나면 == 로드가 완료되면
+    $(window).scroll( function(){ //스크롤 되면 실행
+        $('.des1').each( function(i){ //객체 가져오기
 
-            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight(); //offset == 절대좌표
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1','margin-left':'100px'},1000);
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
             }
         });
     });
@@ -19,7 +19,7 @@ $(document).ready(function() {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             if( bottom_of_window > bottom_of_element ){
-                $(this).animate({'opacity':'1','margin-left':'900px'},1000);
+                $(this).animate({'opacity':'1','margin-left':'0px'},1000);
             }
         });
     });
